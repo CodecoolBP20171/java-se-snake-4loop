c603e561a61fe6b11b98e3126908283eacdb6077
Peter Juhasz <jpeter82@gmail.com>
2017-09-12 14:10:44 +0200

    start menu implementation

--- src/com/codecool/snake/Snake.java
+++ src/com/codecool/snake/Snake.java
@@ -2,6 +2,7 @@ package com.codecool.snake;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Snake extends Application {
@@ -11,7 +12,17 @@ public class Snake extends Application {
         Game game = new Game();
 
         primaryStage.setTitle("Snake Game");
-        primaryStage.setScene(new Scene(game, Globals.WINDOW_WIDTH, Globals.WINDOW_HEIGHT));
+
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, Globals.WINDOW_WIDTH, Globals.WINDOW_HEIGHT);
+        // primaryStage.setScene(new Scene(game, Globals.WINDOW_WIDTH, Globals.WINDOW_HEIGHT));
+
+        // Create menu bar
+        
+
+
+
+
         primaryStage.show();
         game.start();
     }
