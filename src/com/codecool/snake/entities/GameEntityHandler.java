0d2d3d1359fdc33915aef5753f8bc5b2d0faa79b
Peter Juhasz <jpeter82@gmail.com>
2017-09-14 10:46:57 +0200

    Implement entity spawn finished (deleted test comments)

--- src/com/codecool/snake/entities/GameEntityHandler.java
+++ src/com/codecool/snake/entities/GameEntityHandler.java
@@ -40,20 +40,20 @@ public class GameEntityHandler {
     private static void checkEntities() {
 
         if (entityQueue.size() < Globals.MAX_ENTITY_NUMBER) {
-            System.out.println("Entity queue size is less than max entity number");
+
             int possibleNewEntities = Globals.MAX_ENTITY_NUMBER - Globals.actualPowerUps - Globals.actualEnemies;
-            System.out.println("Possible new entities: " + possibleNewEntities);
+
             List<String> missingEntities = new LinkedList<>();
 
             if (possibleNewEntities > 0) {
-                System.out.println("Possible new entities greater than 0");
+
                 int actualNewEntities = Utils.getRandomInt(possibleNewEntities + 1);
-                System.out.println("Random Generated actual new entities: " + actualNewEntities);
+
                 if (actualNewEntities > 0) {
-                    System.out.println("Random Actual new entities greater than 0 this turn");
+
                     int powerupsPossibleMax = Globals.MAX_PUP_NUMBER - Globals.actualPowerUps;
                     int enemiesPossibleMax = Globals.MAX_ENEMY_NUMBER - Globals.actualEnemies;
-                    System.out.println("powerupsPossibleMax: " + powerupsPossibleMax);
+
                     for (int i = 0; i < powerupsPossibleMax; i++) {
                         missingEntities.add("P");
                     }
@@ -66,13 +66,6 @@ public class GameEntityHandler {
                         int randomMissingEntityIndex = Utils.getRandomInt(missingEntities.size());
                         entityQueue.add(missingEntities.remove(randomMissingEntityIndex));
                     }
-
-                    System.out.println("********************************");
-                    System.out.println("entityQueue contains now:");
-                    for (String item : entityQueue) {
-                        System.out.println(item);
-                    }
-                    System.out.println("*********************************");
 
                     addTimer();
 
@@ -90,7 +83,6 @@ public class GameEntityHandler {
 
         if (countdownTimer == 0) {
             countdownTimer = Utils.getRandomTime();
-            System.out.println("New countdownTimer: " + countdownTimer);
         }
 
     }
@@ -117,7 +109,6 @@ public class GameEntityHandler {
                         break;
                 }
 
-
             } else {
 
                 switch (Utils.getRandomInt(3)) {
@@ -138,10 +129,6 @@ public class GameEntityHandler {
 
     public static void decrementEntityCountIfNeeded(String className) {
 
-        System.out.println("Class to decrement: " + className);
-        System.out.println("Old actual powerups: " + Globals.actualPowerUps);
-        System.out.println("Old actual enemies: " + Globals.actualEnemies);
-
         switch (className) {
             case "SimplePowerup": case "MovingPowerup": case "FugitivePowerup":
                 Globals.actualPowerUps--;
@@ -149,9 +136,6 @@ public class GameEntityHandler {
             case "ChasingEnemy": case "ShootingEnemy": case "SimpleEnemy": case "TestEnemy":
                 Globals.actualEnemies--;
         }
-
-        System.out.println("New actual powerups: " + Globals.actualPowerUps);
-        System.out.println("New actual enemies: " + Globals.actualEnemies);
 
     }
 
